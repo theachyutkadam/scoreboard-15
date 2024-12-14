d0b634abfefa16ed8a154aa6cd04c9799db132fa
achyutkadamacl <achyut.kadam@acldigital.com>
2024-12-14 18:38:16 +0530

    Added connection service

---
+++ src/app/connections/config.ts
@@ -0,0 +1,3 @@
+// API BASE URI
+export const API_BASE_URL = 'http://192.168.1.40:3000/'
+export const API_URL = 'http://192.168.1.40:3000'
---
+++ src/app/connections/http.service.ts
@@ -0,0 +1,57 @@
+import { Injectable } from '@angular/core';
+import { API_URL, API_BASE_URL } from './config';
+import { Observable } from 'rxjs/internal/Observable';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpService {
+
+  AUTH_TOKEN: any = ''
+  constructor(private _http: HttpClient) { }
+
+  httpHeaderWithToken = new HttpHeaders({
+    'Accept': 'application/json',
+    'content-type': 'application/json',
+    'Authorization': `Bearer ${this.AUTH_TOKEN}`,
+  })
+
+  get(url: any, params: any = null): Observable<any> {
+    const configured_params = this.configureParams(params)
+    return this._http.get(`${API_BASE_URL}${url}${configured_params}`, { headers: this.httpHeaderWithToken })
+  }
+
+  post(url: any, payload: any, params: any = null) {
+    const configured_params = this.configureParams(params)
+    return this._http.post(`${API_BASE_URL}${url}${configured_params}`, payload, {headers: this.httpHeaderWithToken})
+  }
+
+  put(url: any, payload: any) {
+    return this._http.put(`${API_BASE_URL}${url}`, payload, {headers: this.httpHeaderWithToken})
+  }
+
+  patch(url: any, payload: any) {
+    return this._http.patch(`${API_BASE_URL}${url}`, payload, {headers: this.httpHeaderWithToken})
+  }
+
+  delete(url: any) {
+    return this._http.delete(`${API_BASE_URL}${url}`, {headers: this.httpHeaderWithToken})
+  }
+
+  configureParams(params: any) {
+    let params_string = ""
+    if (params) {
+      params.forEach((element: any, index: number) => {
+        if (index == 0) {
+          params_string += `?${element.key}=${element.value}`
+        }
+
+        if (index > 0 && params.length > 1) {
+          params_string += `&${element.key}=${element.value}`
+        }
+      });
+    }
+    return params_string
+  }
+}

c0a7da260fbe453151b2c645415fa2dc7a5a98b3
ak <theachyutkadam@gmail.com>
2025-01-04 14:30:11 +0530

    Update Config backend URL

--- src/app/connections/config.ts
+++ src/app/connections/config.ts
@@ -1,4 +1,8 @@
 // API BASE URI
-export const API_BASE_URL = 'http://192.168.1.40:3000/'
-export const API_URL = 'http://192.168.1.40:3000'
+// export const API_BASE_URL = 'http://192.168.1.40:3000/'
+// export const API_URL = 'http://192.168.1.40:3000'
+
+// AWS Host URL
+export const API_BASE_URL = 'http://54.175.186.226:3000/'
+export const API_URL = 'http://54.175.186.226:3000'
 export const authToken = sessionStorage.getItem('authToken');

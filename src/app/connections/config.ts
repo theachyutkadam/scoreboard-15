7ce679bdace52b5180e54a903481297ca20629a7
theachyutkadam <theachyutkadam@gmail.com>
2025-01-15 16:18:58 +0530

    Update hosting

--- src/app/connections/config.ts
+++ src/app/connections/config.ts
@@ -1,6 +1,11 @@
 // API BASE URI
+// R16 wifi
 export const API_BASE_URL = 'http://192.168.1.40:3000/'
 export const API_URL = 'http://192.168.1.40:3000'
+
+// R6 wifi
+// export const API_BASE_URL = 'http://192.168.31.168:3000/'
+// export const API_URL = 'http://192.168.31.168:3000'
 
 // AWS Host URL
 // export const API_BASE_URL = 'http://54.175.186.226:3000/'

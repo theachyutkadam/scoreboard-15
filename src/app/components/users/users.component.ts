c7790f25e74771f4013ca008ca31a1d4eeafc0d1
theachyutkadam <theachyutkadam@gmail.com>
2025-02-11 00:48:37 +0530

    Set center table to players linsting

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -16,21 +16,13 @@ export class UsersComponent {
 
   user_id: any
   players: any
+
+  //table head asc and desc variables
+  order: boolean = false
+  order_by: string = 'id'
+
+  //table data variables
   users: any
-
-  constructor(
-    private http: HttpService,
-    private toastr: ToastrService,
-    private common: CommonTaskService
-  ){}
-
-  ngOnInit(){ this.getPlayers() }
-
-  getPlayers(){ // TODO: Call players API bcz User Details are save in players table
-    this.http.get('players', '').subscribe((response: any) => {
-      this.users = response.players
-    })
-  }
 
   form_fields = {
     // email: ['', Validators.required],
@@ -53,6 +45,49 @@ export class UsersComponent {
     { type: 'select', is_required: true, label: 'Gender' , form_control_name: 'gender', dropdown: this.common.genders },
     { type: 'select', is_required: true, label: 'Speciality' , form_control_name: 'speciality', dropdown: this.common.specialities},
   ]
+
+  headers = [
+    {name: "ID", field: 'id', type: 'number'},
+    {name: "Name", field: 'name', type: 'string'},
+    {name: "Email", field: 'email', type: 'object'},
+    {name: "Contact", field: 'contact', type: 'string'},
+    {name: "Speciality", field: 'speciality', type: 'string'},
+    {name: "Gender", field: 'gender', type: 'string'},
+    // {name: "Status", field: 'user.status', type: 'object'},
+  ]
+
+  filter_options = [
+    {name: "Batsman", value: "batsman"},
+    {name: "Bowler", value: "bowler"},
+    {name: "Filder", value: "filder"},
+    {name: "All", value: ""},
+  ]
+
+  constructor(
+    private http: HttpService,
+    private toastr: ToastrService,
+    private common: CommonTaskService
+  ){}
+
+  ngOnInit(){ this.getPlayers() }
+
+  getPlayersByOrder(event: any = "first_name") {
+    console.log('-CT sort-->', event);
+    this.order = !this.order
+    this.order_by = event.sort_by == "name" ? 'first_name' : event.sort_by
+    this.getPlayers(event.filter)
+  }
+
+  getPlayers(event: any= "batsman"){ // TODO: Call players API bcz User Details are save in players table
+    let params = [
+      {key: "speciality", value: event},
+      {key: "order_by", value: this.order_by},
+      {key: "order", value: this.order ? 'asc' : 'desc'}
+    ]
+    this.http.get('players', params).subscribe((response: any) => {
+      this.users = response.players
+    })
+  }
 
   setPayload(event: any){
     let payload = {
@@ -85,6 +120,10 @@ export class UsersComponent {
     this.getPlayers()
     this.edit_user = null
     this.toastr.success(msg, 'Success!');
+  }
+
+  tableAction(event: any){
+    event.action == 'Edit' ? this.editUser(event.record_id) : this.deleteUser(event.record_id)
   }
 
   editUser(user_id: any){
